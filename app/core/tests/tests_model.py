8791387dc5ddfd49b653a1c94c93bc284b32102e
Hamza09s <hamzaaqeel09@gmail.com>
2023-03-30 12:10:44 +0500

    before adding self.normalize_email

--- app/core/tests/tests_model.py
+++ app/core/tests/tests_model.py
@@ -35,17 +35,27 @@ class ModelTests(TestCase):
             self.assertEqual(user.email, expected)
 
         # according chatgpt
-        # In the context of the provided Django test code, the test_new_user_email_normalized
-        # method tests whether email addresses are properly normalized when new users are created.
-        # Email address normalization is the process of converting an email address to a
-        # standard format, typically all lowercase letters, to ensure consistency and avoid
-        #  confusion caused by different capitalization of the same email address. For
-        # example, the email address 'JohnDoe@example.com' would be normalized to 'johndoe@example.com'.
-        # In the test_new_user_email_normalized method, the sample_emails list contains
-        # pairs of email addresses and their expected normalized versions. The method
-        # iterates over each pair and creates a new user with the provided email address
-        # using the create_user method. It then checks whether the created user's email
+
+        # Email address normalization is the process of converting
+        #  an email address to a
+        # standard format, typically all lowercase letters,
+        # to ensure consistency and avoid
+        #  confusion caused by different capitalization of the
+        # same email address. For
+        # example, the email address 'JohnDoe@example.com' would be
+        #  normalized to 'johndoe@example.com'.
+        # In the test_new_user_email_normalized method,
+        # the sample_emails list contains
+        # pairs of email addresses and their expected normalized
+        # versions. The method
+        # iterates over each pair and creates a new user with the
+        # provided email address
+        # using the create_user method. It then checks
+        # whether the created user's email
         # address matches the expected normalized version.
-        # The different capitalization in the sample_emails list is intentional, as email
-        # addresses are case-insensitive. Different email providers may handle
-        # capitalization differently, but they all treat the same email address with different capitalization as equivalent. Therefore, to ensure consistency and avoid confusion, the email address should be normalized to a standard format, such as all lowercase letters.
+        # The different capitalization in the sample_emails list is
+        # intentional, as email
+        # addresses are case-insensitive. Different email providers
+        # may handle
+        # capitalization differently, but they all treat the same email
+        # address with different capitalization as equivalent. Therefore, to ensure consistency and avoid confusion, the email address should be normalized to a standard format, such as all lowercase letters.
