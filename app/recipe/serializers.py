148a3fded791da6178b5c5cfe99f9ba5102ff362
Hamza09s <hamzaaqeel09@gmail.com>
2023-04-04 16:53:37 +0500

    in lesson 100

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -8,21 +8,7 @@ from core.models import (
     Tag,
 )
 
-
-class RecipeSerializer(serializers.ModelSerializer):
-    """Serializer for recipes."""
-
-    class Meta:
-        model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link', ]
-        read_only_fields = ['id']
-
-
-class RecipeDetailSerializer(RecipeSerializer):
-    """Serializer for recipe detail view."""
-
-    class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
+# moved here because we want to nest it in recipe serializer
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -32,3 +18,41 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
         fields = ['id', 'name']
         read_only_fields = ['id']
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for recipes."""
+    tags = TagSerializer(many=True, required=False)
+    # we have made tags an optional part of our recipe
+    # many=True means this is gonna be a list of items
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
+        read_only_fields = ['id']
+
+    def create(self, validated_data):
+        """Create a recipe."""
+        tags = validated_data.pop('tags', [])
+        # ingredients = validated_data.pop('ingredients', [])
+        recipe = Recipe.objects.create(**validated_data)
+        self._get_or_create_tags(tags, recipe)
+        # self._get_or_create_ingredients(ingredients, recipe)
+
+        return recipe
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail view."""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
+
+
+# class TagSerializer(serializers.ModelSerializer):
+#     """Serializer for tags."""
+
+#     class Meta:
+#         model = Tag
+#         fields = ['id', 'name']
+#         read_only_fields = ['id']
